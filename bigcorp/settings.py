c6571f2ad39978371d3e999c6e74d8fb6f8f570f
Daniel <danielmello0110@gmail.com>
2025-09-12 16:08:22 -0300

    Fix settings: update ALLOWED_HOSTS to remove protocol from domain

--- bigcorp/settings.py
+++ bigcorp/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     ".render.com",
-    "https://back-fedconnect-y46st.ondigitalocean.app",
+    "back-fedconnect-y46st.ondigitalocean.app"
     
     
     
