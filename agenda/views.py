48ed80879bd09779f3042f89a6f081c9fe59162c
Daniel <danielmello0110@gmail.com>
2025-09-24 15:25:31 -0300

    Ajuste agenda

--- agenda/views.py
+++ agenda/views.py
@@ -12,6 +12,18 @@ class ReservaViewSet(viewsets.ModelViewSet):
         IsAuthenticated
     ]  # Garante que apenas usuários logados possam interagir
 
+    def get_queryset(self):
+        """
+        Sobrescreve o queryset para filtrar por um período de datas.
+        """
+        queryset = super().get_queryset()
+        data_inicio = self.request.query_params.get('data_inicio')
+        data_fim = self.request.query_params.get('data_fim')
+
+        if data_inicio and data_fim:
+            queryset = queryset.filter(data__range=[data_inicio, data_fim])
+        return queryset.order_by('data', 'horario')
+
     def perform_create(self, serializer):
-        # Atribuir o usuário logado como criador da reserva
+        # Atribui o usuário logado como criador da reserva
         serializer.save(criado_por=self.request.user)
