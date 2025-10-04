d49618461801646c200ad9dcc7dcc32667fcb148
Daniel Mello <danielmello0110@gmail.com>
2025-10-04 08:09:20 -0300

    ajustes para cancelamento

--- agenda_comercial/models.py
+++ agenda_comercial/models.py
@@ -3,14 +3,17 @@
 from django.db import models
 from bigcorp import settings
 
+
 class Agendamento(models.Model):
     empresa = models.CharField(max_length=200)
     data = models.DateField()
-    responsavel = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='agendamentos')
-    status = models.CharField(max_length=20, default='Pendente')
+    responsavel = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="agendamentos"
+    )
+    status = models.CharField(max_length=20, default="Pendente")
     hora = models.TimeField(null=True, blank=True)
     obs = models.TextField(null=True, blank=True)
-
+    motivo_cancelamento = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f'Agendamento com {self.empresa} em {self.data}'
+        return f"Agendamento com {self.empresa} em {self.data}"
--- agenda_comercial/serializers.py
+++ agenda_comercial/serializers.py
@@ -4,9 +4,19 @@ from rest_framework import serializers
 from .models import Agendamento
 from users.serializers import UsuarioSerializer
 
+
 class AgendamentoSerializer(serializers.ModelSerializer):
     responsavel = UsuarioSerializer(read_only=True)
 
     class Meta:
         model = Agendamento
-        fields = ['id', 'empresa', 'data', 'responsavel', 'status', 'hora', 'obs']
+        fields = [
+            "id",
+            "empresa",
+            "data",
+            "responsavel",
+            "status",
+            "hora",
+            "obs",
+            "motivo_cancelamento",
+        ]
