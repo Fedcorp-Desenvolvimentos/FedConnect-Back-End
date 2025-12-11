feae93e8b8a539e5407fe31f8a156340f37d0a1d
Leonan Thomaz <leonan.thomaz@gmail.com>
2025-12-11 10:17:40 -0300

    Adicionando config alternativa - env

--- bigcorp/settings.py
+++ bigcorp/settings.py
@@ -73,6 +73,18 @@ DATABASES = {
         "PORT": os.environ.get("port"),
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": os.environ.get("DB_PORT"),
+#     }
+# }
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
