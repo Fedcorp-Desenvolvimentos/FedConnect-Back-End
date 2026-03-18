5d99c942914ee1ccfcfea2f4908e16f36331fca3
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-03-18 11:18:01 -0300

    Amplicando campos - Santander Webhook

--- bank/views.py
+++ bank/views.py
@@ -11,9 +11,56 @@ class SantanderWebhookView(APIView):
 
     def get(self, request):
         return Response(status=200)
-
+    
+       
     def post(self, request):
         data = request.data
+
         logger.info(f"Webhook recebido: {data}")
-        print("Webhook recebido:", data)
+
+        # 🔑 tipo de pagamento
+        payment_type = data.get("paymentType")
+
+        # 🔑 identificador principal
+        if payment_type == "PIX":
+            identificador = data.get("txId")
+        else:
+            identificador = data.get("bankNumber")
+
+        # 💰 valores
+        valor_pago = data.get("payedValue")
+        valor_nominal = data.get("nominalValue")
+
+        # 👤 pagador
+        pagador_nome = data.get("payerName")
+        pagador_doc = data.get("payerDocumentNumber")
+
+        # 📅 datas
+        data_pagamento = data.get("paymentDate")
+        data_credito = data.get("creditDate")
+
+        # 🧾 controle interno
+        nosso_numero = data.get("clientNumber")
+        convenio = data.get("covenant")
+
+        logger.info(
+            f"""
+            🔔 PAGAMENTO RECEBIDO
+            tipo={payment_type}
+            valor_nominal={valor_nominal}
+            valor_pago={valor_pago}
+            data_pagamento={data_pagamento}
+            data_credito={data_credito}
+            convenio={convenio}
+            id={identificador}
+            nosso_numero={nosso_numero}
+            valor={valor_pago}
+            pagador={pagador_nome} ({pagador_doc})
+            """
+        )
+
+        if not identificador and not nosso_numero:
+            logger.warning("Webhook sem identificador válido")
+            return Response(status=200)
+
         return Response(status=200)
